4410df08a82be582e0f8cfa147de0fa785ffb4a9
Laur dArmien <n3v3r8g81n@gmail.com>
2019-09-25 00:51:25 +0000

    lesson 15 done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_scope :users do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :courses, only: [:index, :show]
   namespace :instructor do
     resources :courses, only: [:new, :create, :show]
 end
