68d74ab9f265af27897f4c316a36cc94533881bb
Laur dArmien <n3v3r8g81n@gmail.com>
2019-10-21 18:40:05 +0000

    middle of lesson 18, courses_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   end
   resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
+    resources :sections, only: [] do
+        resources :lessons, only: [:new, :create]
+      end
+          resources :courses, only: [:new, :create, :show] do
+            resources :sections, only: [:new, :create]
+        end
     end
-end
 end
